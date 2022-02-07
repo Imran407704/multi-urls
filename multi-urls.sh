f8f78c7d2c3556eac7e317fc098841c385f88aa2
Imran Shaikh <51431951+Imran407704@users.noreply.github.com>
2022-02-07 16:46:23 +0000

    Update multi-urls.sh

--- multi-urls.sh
+++ multi-urls.sh
@@ -16,24 +16,29 @@ echo -e $Yellow "
 
 target=$1
 printf "Total Targets: $(wc -l $1 | awk '{print $1}')\n"
-
+mkdir -p output-multi_url
 echo "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
 printf "Waybackurls Started\n"
-cat $1 | xargs -P 100 -I X bash -c "echo 'X' | waybackurls | tee waybackurls.txt > /dev/null"
-printf "✅ Waybackurls data :$(wc -l waybackurls.txt | awk '{print $1}')\n"
+cat $1 | xargs -P 100 -I X bash -c "echo 'X' | waybackurls > output-multi_url/waybackurls.txt > /dev/null"
+printf "✅ Waybackurls data :$(wc -l output-multi_url/waybackurls.txt | awk '{print $1}')\n"
 
 echo "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
 printf "Gau-Plus Started\n"
-cat $1 | gauplus --random-agent -t 100 -o plus-gau.txt
-printf "✅ Plus-gau data :$(wc -l plus-gau.txt | awk '{print $1}')\n"
+cat $1 | gauplus --random-agent -t 100 -o output-multi_url/plus-gau.txt
+printf "✅ Plus-gau data :$(wc -l output-multi_url/plus-gau.txt | awk '{print $1}')\n"
 
 echo "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
 printf "Gau Data Started\n"
-cat $1 | gau > gau.txt
-printf "✅ Gau data :$(wc -l gau.txt | awk '{print $1}')\n"
+cat $1 | gau --o output-multi_url/gau.txt
+printf "✅ Gau data :$(wc -l output-multi_url/gau.txt | awk '{print $1}')\n"
 
-cat waybackurls.txt plus-gau.txt gau.txt | sort -u > uniq-data.txt
+cat output-multi_url/waybackurls.txt output-multi_url/plus-gau.txt output-multi_url/gau.txt | sort -u > output-multi_url/all-urls.txt
 
-printf "Total Uniq-Data :$(wc -l uniq-data.txt)\n"
+cat output-multi_url/all-urls.txt | httpx > output-multi_url/live-multi_url.txt
+
+echo "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
+
+printf "Total all-urls :$(wc -l output-multi_url/all-urls.txt)\n"
+printf "Total live-multi_url :$(wc -l output-multi_url/live-multi_url.txt)\n"
 
 echo "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
